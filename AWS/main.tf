99e242f2abdf91cedc5d9994f8eac59039318d6e
warnerb47 <taphagaye2001.mg@gmail.com>
2022-08-11 20:49:05 +0000

    first commit

---
+++ AWS/main.tf
@@ -0,0 +1,85 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# load config file
+locals {
+  input_file         = "./config.yml"
+  input_file_content = fileexists(local.input_file) ? file(local.input_file) : "NoInputFileFound: true"
+  input              = yamldecode(local.input_file_content)
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = local.input.access.region
+  access_key = local.input.access.access_key
+  secret_key = local.input.access.secret_key
+}
+
+
+
+# filter image 
+data "aws_ami" "filtred_amis" {
+  most_recent = true
+
+  count = length(local.input.ec2_instances)
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "name"
+    values = [local.input.ec2_instances[count.index].name]
+  }
+
+  filter {
+    name   = "architecture"
+    values = [local.input.ec2_instances[count.index].architecture]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = [local.input.ec2_instances[count.index].virtualization-type]
+  }
+
+  owners = [local.input.ec2_instances[count.index].owner]
+
+}
+
+# get exiting key pair
+data "aws_key_pair" "access_key_name" {
+  key_name = local.input.access.key_name
+}
+
+# create ec2 instances
+resource "aws_instance" "ec2_instances" {
+  count           = length(data.aws_ami.filtred_amis)
+  ami             = data.aws_ami.filtred_amis[count.index].id
+  instance_type   = local.input.ec2_instances[count.index].instance_type
+  key_name        = data.aws_key_pair.access_key_name.key_name
+  security_groups = local.input.ec2_instances[count.index].security_group
+  tags = {
+    Name = local.input.ec2_instances[count.index].tag
+  }
+}
+
+# print result 
+output "ec2_instances_created" {
+  value = {
+    for k, v in aws_instance.ec2_instances : k => {
+      public_ip       = v.public_ip
+      public_dns      = v.public_dns
+      tags            = v.tags
+      security_groups = v.security_groups
+    }
+  }
+}
---
+++ AWS/variables.tf
@@ -0,0 +1,51 @@
+variable "access" {
+  type = object({
+    region     = string
+    access_key = string
+    secret_key = string
+    key_name   = string
+    vpc_id     = string
+  })
+  default = {
+    region     = "value"
+    access_key = "value"
+    secret_key = "value"
+    key_name   = "value"
+    vpc_id     = "value"
+  }
+}
+
+variable "ansible" {
+  type = object({
+    public_key = string
+  })
+  default = {
+    public_key = "value"
+  }
+}
+
+
+variable "ec2_instances" {
+  type = list(object({
+    owner               = string
+    platform            = string
+    architecture        = string
+    virtualization-type = string
+    name                = string
+    instance_type       = string
+    tag                 = string
+    security_group      = string
+  }))
+  default = [
+    {
+      owner               = "value"
+      platform            = "value"
+      architecture        = "value"
+      virtualization-type = "value"
+      name                = "value"
+      instance_type       = "value"
+      tag                 = "value"
+      security_group      = "value"
+    }
+  ]
+}
